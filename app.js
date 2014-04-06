a42e5d48584f4b143a3800ebba720449445c70d9
Dean Sofer <proloser@hotmail.com>
2014-04-05 23:20:33 -0700

    More code and shit

--- app.js
+++ app.js
@@ -3,17 +3,29 @@ app.config(function($stateProvider, $urlRouterProvider){
     $urlRouterProvider.otherwise('/');
     $stateProvider.state('home', {
         url: '/',
-        templateUrl: 'home.html'
+        templateUrl: 'home.html',
+        controller: function($scope) {
+            function S4() {
+               return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+            }
+            function guid() {
+               return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+            }
+            $scope.id = guid();
+        }
     })
     $stateProvider.state('script', {
         url: '/:scriptId',
         templateUrl: 'script.html',
         controller: function($scope, $firebase, $stateParams){
-             var script = new Firebase("https://screenwrite.firebaseio.com/"+$stateParams.scriptId);
+            var script = new Firebase("https://screenwrite.firebaseio.com/"+$stateParams.scriptId);
             fbScript = $firebase(script);
             fbScript.$bind($scope, 'script', function(){
-                if (!$scope.script.lines)
-                    $scope.$apply("script.lines = [{type:'scene'}]");
+                if (!$scope.script.lines) {
+                    $scope.$apply(function(){
+                        $scope.script.lines = [{type:'scene'}];
+                    });
+                }
             });
           
             var types = $scope.types = ['scene', 'action', 'character', 'dialogue', 'parenthetical', 'transition', 'shot', 'text'];
@@ -36,6 +48,12 @@ app.config(function($stateProvider, $urlRouterProvider){
             };
             $scope.keydown = function($event, line){
                 switch ($event.keyCode) {
+                    case 8: // backspace
+                        if (!line.text) {
+                            $scope.script.lines.splice($scope.script.lines.indexOf(line), 1);
+                            $event.preventDefault();
+                        }
+                        break;
                     case 9: // tab
                         $event.preventDefault();
                         if ($event.shiftKey) {
@@ -74,6 +92,7 @@ app.directive('textarea', function(){
         restrict: 'E',
         link: function($scope, $element, $attrs) {
             $element.on('input', function(){
+                $element[0].style.height = $element[0].scrollHeight + 'px';
             });
         }
     };
