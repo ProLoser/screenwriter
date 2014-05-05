edc71d1356ee929344a4e5f798e7158083dc99b9
Dean Sofer <proloser@hotmail.com>
2014-05-05 04:16:33 -0700

    Working on new bootstrap interface

--- app.js
+++ app.js
@@ -1,169 +1,195 @@
-app = angular.module('scrnplay', ['ui.router', 'firebase']);
+app = angular.module('scrnplay', ['ui.router', 'firebase', 'ngStorage']);
 app.config(function($stateProvider, $urlRouterProvider){
     $urlRouterProvider.otherwise('/');
     $stateProvider.state('home', {
         url: '/',
-        templateUrl: 'home.html',
-        controller: function($scope) {
-            function S4() {
-               return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
-            }
-            function guid() {
-               return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
-            }
-            $scope.id = guid();
-        }
-    })
+        templateUrl: 'home'
+    });
     $stateProvider.state('script', {
-        url: '/:scriptId',
-        templateUrl: 'script.html',
-        controller: function($scope, $firebase, $stateParams, $timeout){
-            var script = new Firebase("https://screenwrite.firebaseio.com/"+$stateParams.scriptId);
-            fbScript = $firebase(script);
-            fbScript.$bind($scope, 'script', function(){
-                $scope.$apply(function(){
-                    if (!$scope.script)
-                        $scope.script = {};
-                    if (!$scope.script.lines)
-                        $scope.script.lines = [{type:'scene'}];
+        url: '/{scriptId:.+}',
+        resolve: {
+            script: function($rootScope, $stateParams, $firebase, $localStorage){
+                var script = new Firebase("https://screenwrite.firebaseio.com/"+$stateParams.scriptId);
+                fbScript = $firebase(script);
+                return fbScript.$bind($rootScope, 'script').then(function(unbind){
+                    if (!$rootScope.script)
+                        $rootScope.script = {};
+                    if (!$rootScope.script.lines)
+                        $rootScope.script.lines = [{type:'scene'}];
+                    $localStorage[$stateParams.scriptId] = $rootScope.script;
+                    return unbind;
                 });
-            });
-          
-            var types = $scope.types = ['scene', 'action', 'character', 'dialogue', 'parenthetical', 'transition', 'shot', 'text'];
-            var nextTypes = {
-                scene: 'action',
-                action: 'action',
-                character: 'dialogue',
-                dialogue: 'character',
-                parenthetical: 'dialogue',
-                transition: 'scene',
-                shot: 'action',
-                text: 'text'
-            };
-            $scope.comment = function(line){
-                if ($scope.commenting === line) {
-                    $scope.commenting = null;
-                } else {
-                    $scope.commenting = line;
-                    $scope.$broadcast('comment', line);
-                }
-            };
-            $scope.edit = function(line){
-                $scope.editing = line;
-            };
-            $scope.keypress = function($event, line){
-                switch ($event.keyCode) {
-                    case 13: // enter
-                        $event.preventDefault();
-                        if (line.text) {
-                            $scope.newLine(line);
-                        }
-                }
-            };
-            
-            $scope.focus = function(line) {
-                $scope.$broadcast('focus', line);
             }
-            $scope.clean = function(text) {
-                return text.replace(/[^0-9a-zA-Z]/, '+');
-            };
-            $scope.suggestions = function(line) {
-                if (line.type !== 'character' && line.type !== 'scene') return;
-                var suggestions = [];
-                $scope.script.lines.forEach(function(suggestion){
-                    if ( line.type === suggestion.type && suggestion.text.toUpperCase() !== line.text.toUpperCase() && ~suggestion.text.toUpperCase().indexOf(line.text.toUpperCase()) && !~suggestions.indexOf(suggestion.text) )
-                        suggestions.push(suggestion.text);
-                });
-                return suggestions;
-            };
-            $scope.keydown = function($event, line){
-                switch ($event.keyCode) {
-                    case 38: // up
-                        if ($event.shiftKey) {
-                            var lines = $scope.script.lines;
-                            var index = lines.indexOf(line);
-                            if (index > 0) {
-                                lines.splice(index, 1);
-                                lines.splice(index - 1, 0, line);
-                                $timeout(function(){
-                                    $scope.focus(line); 
-                                });
-                            }
-                        } else {
-                            $scope.focus($scope.script.lines[$scope.script.lines.indexOf(line) - 1]);
-                            $event.preventDefault();
-                        }
-                        break;
-                    case 40: // down
-                        if ($event.shiftKey) {
-                            var lines = $scope.script.lines;
-                            var index = lines.indexOf(line);
-                            lines.splice(index, 1);
-                            lines.splice(index + 1, 0, line);
-                            $timeout(function(){
-                                $scope.focus(line); 
-                            });
-                        } else {
-                            $scope.focus($scope.script.lines[$scope.script.lines.indexOf(line)+1]);
-                            $event.preventDefault();
-                        }
-                        break;
-                    case 8: // backspace
-                        if (!line.text && $scope.script.lines.length > 1) {
-                            $scope.focus($scope.script.lines[$scope.script.lines.indexOf(line)-1]);
-                            $scope.script.lines.splice($scope.script.lines.indexOf(line), 1);
-                            $event.preventDefault();
-                        }
-                        break;
-                    case 13: // enter
-                        if (line.text) {
-                            break;
-                        } else {
-                            $event.preventDefault();
-                        }
-                    case 9: // tab
-                        $event.preventDefault();
-                        if ($event.shiftKey) {
-                            $scope.prev(line);
-                        } else {
-                            $scope.next(line);
-                        }
-
-                }
-            };
-            $scope.newLine = function(line) {
-                var newLine = {
-                    type: nextTypes[line.type]
-                };
-                $scope.script.lines.splice($scope.script.lines.indexOf(line) + 1, 0, newLine);
-                $scope.edit(newLine);
-            };
-            $scope.next = function(line) {
-                var type = types.indexOf(line.type) + 1;
-                type = (type < types.length) ? type : 0;
-                line.type = types[type];
-            };
-            $scope.prev = function(line) {
-                var type = types.indexOf(line.type) - 1;
-                type = (type >= 0) ? type : types.length - 1;
-                line.type = types[type];
-            };
-            $scope.words = function(){
-                var words = 0;
-                $scope.script.lines.forEach(function(line){
-                    words += line.text.split(' ');
-                });
-                return words;
+        },
+        views: {
+            'nav': {
+                templateUrl: 'nav.html',
+                controller: 'Nav',
+            },
+            '': {
+                templateUrl: 'script.html',
+                controller: 'Script',
             }
-            
+        },
+        onExit: function($rootScope, script) {
+            // unbind 
+            script();
+            $rootScope.script = null;
         }
     });
+});
+app.run(function($rootScope, $state, $timeout, types){
+    $rootScope.types = types;
+
+    $rootScope.dropdowns = {};
+
+    function S4() {
+       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    }
+    function guid() {
+       return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+    }
+    $rootScope.new = function(){
+        $state.go('script', { scriptId: guid() });
+    };
+
+    $rootScope.edit = function(line){
+        $rootScope.editing = line;
+    };
+    $rootScope.focus = function(line){
+        $rootScope.$broadcast('focus', line);
+    };
+});
+app.constant('types', ['scene', 'action', 'character', 'dialogue', 'parenthetical', 'transition', 'shot', 'text']);
+app.controller('Nav', function($scope){
+});
+app.controller('Script', function($scope){
+    var nextTypes = {
+        scene: 'action',
+        action: 'action',
+        character: 'dialogue',
+        dialogue: 'character',
+        parenthetical: 'dialogue',
+        transition: 'scene',
+        shot: 'action',
+        text: 'text'
+    };
+    $scope.comment = function(line){
+        if ($scope.commenting === line) {
+            $scope.commenting = null;
+        } else {
+            $scope.commenting = line;
+            $scope.$broadcast('comment', line);
+        }
+    };
+    $scope.keypress = function($event, line){
+        switch ($event.keyCode) {
+            case 13: // enter
+                $event.preventDefault();
+                if (line.text) {
+                    $scope.newLine(line);
+                }
+        }
+    };
+    $scope.clean = function(text) {
+        return text.replace(/[^0-9a-zA-Z]/, '+');
+    };
+    $scope.suggestions = function(line) {
+        if (line.type !== 'character' && line.type !== 'scene') return;
+        var suggestions = [];
+        $scope.script.lines.forEach(function(suggestion){
+            if ( suggestion.text && line.text && line.type === suggestion.type && suggestion.text.toUpperCase() !== line.text.toUpperCase() && ~suggestion.text.toUpperCase().indexOf(line.text.toUpperCase()) && !~suggestions.indexOf(suggestion.text) )
+                suggestions.push(suggestion.text);
+        });
+        return suggestions;
+    };
+    $scope.keydown = function($event, line){
+        switch ($event.keyCode) {
+            case 38: // up
+                if ($event.shiftKey) {
+                    var lines = $scope.script.lines;
+                    var index = lines.indexOf(line);
+                    if (index > 0) {
+                        lines.splice(index, 1);
+                        lines.splice(index - 1, 0, line);
+                        $scope.focus(line); 
+                    }
+                } else {
+                    $scope.focus($scope.script.lines[$scope.script.lines.indexOf(line) - 1]);
+                }
+                $event.preventDefault();
+                break;
+            case 40: // down
+                if ($event.shiftKey) {
+                    var lines = $scope.script.lines;
+                    var index = lines.indexOf(line);
+                    lines.splice(index, 1);
+                    lines.splice(index + 1, 0, line);
+                    $scope.focus(line); 
+                } else {
+                    $scope.focus($scope.script.lines[$scope.script.lines.indexOf(line)+1]);
+                }
+                $event.preventDefault();
+                break;
+            case 8: // backspace
+                if (!line.text && $scope.script.lines.length > 1) {
+                    $scope.focus($scope.script.lines[$scope.script.lines.indexOf(line)-1]);
+                    $scope.script.lines.splice($scope.script.lines.indexOf(line), 1);
+                    $event.preventDefault();
+                }
+                break;
+            case 13: // enter
+                if (line.text) {
+                    break;
+                } else {
+                    $event.preventDefault();
+                }
+            case 9: // tab
+                $event.preventDefault();
+                if ($event.shiftKey) {
+                    $scope.prev(line);
+                } else {
+                    $scope.next(line);
+                }
+
+        }
+    };
+    $scope.newLine = function(line) {
+        var newLine;
+        var nextLine = $scope.script.lines[$scope.script.lines.indexOf(line) + 1];
+        if (nextLine && !nextLine.text) {
+            newLine = nextLine;
+        } else {
+            newLine = {
+                type: nextTypes[line.type]
+            };
+            $scope.script.lines.splice($scope.script.lines.indexOf(line) + 1, 0, newLine);
+        }
+        $scope.focus(newLine);
+    };
+    $scope.next = function(line) {
+        var type = types.indexOf(line.type) + 1;
+        type = (type < types.length) ? type : 0;
+        line.type = types[type];
+    };
+    $scope.prev = function(line) {
+        var type = types.indexOf(line.type) - 1;
+        type = (type >= 0) ? type : types.length - 1;
+        line.type = types[type];
+    };
+    $scope.words = function(){
+        var words = 0;
+        $scope.script.lines.forEach(function(line){
+            words += line.text.split(' ');
+        });
+        return words;
+    }
+    
 });
 app.directive('textarea', function($timeout){
     return {
         restrict: 'E',
         link: function($scope, $element, $attrs) {
-            $element[0].focus();
             setTimeout(function(){
                 $element[0].style.height = $element[0].scrollHeight + 'px';
             });
@@ -181,9 +207,7 @@ app.directive('textarea', function($timeout){
             if ($element.hasClass('line')) {
                 $scope.$on('focus', function(event, line){
                    if (line === $scope.line)
-                        setTimeout(function(){
-                            $element[0].focus(); 
-                        });
+                        $element[0].focus(); 
                 });
             }
             if ($element.hasClass('comment')) {
