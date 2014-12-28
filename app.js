1f98acc157eee08bc2bffd2cf46bdb3aa14d89d8
Dean Sofer <dean@Deans-MacBook-Pro.local>
2014-12-27 19:12:34 -0800

    Allow toggling of dropdowns

--- app.js
+++ app.js
@@ -48,6 +48,13 @@ app.run(function($rootScope, $state, types, $timeout, $window){
         $window.print();
     };
 
+    $rootScope.toggle = function(dropdown) {
+        if ($rootScope.dropdowns === dropdown)
+            $rootScope.dropdowns = '';
+        else
+            $rootScope.dropdowns = dropdown;
+    };
+
     function S4() {
        return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
     }
